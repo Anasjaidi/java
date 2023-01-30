2045142783f4bbfef2ae4512e0eb575240c48a36
Anas jaidi <anas.jaidi@icloud.com>
2023-01-30 23:34:07 +0100

    add all types

--- components/print.java
+++ components/print.java
@@ -1,6 +1,7 @@
+package components;
 public class print {
   static public void main(String[] ergs) {
     System.out.print("Hello, World!\n");
-    System.out.print("anas jaidi");
+    System.out.println("anas jaidi");
   }
 }
---
+++ components/vars.java
@@ -0,0 +1,15 @@
+package components;
+
+public class vars {
+  public static void main(String[] args) {
+    boolean bol = true; // primitive, 1b, (true, false)
+    byte by = 12; // primitive, 1b, (-128, 127)
+    short sh = 12; // primitive, 2b, (-32k, 32k)
+    int i = 12; // primitive, 4b, (-2b, 2b)
+    long l = 12; // primitive, 8b, (-9q, 9q)
+    float f = 3.14125f; // primitive, 4b, (up to 6-5 digit fraction)
+    double d = 3.1423456789; // primitive, 8b, (up to 16-15 digit fraction)
+    char c = 'a'; // primitive 1b, ascii
+    String s = "anas jaidi\n"; // ref
+  }
+}
