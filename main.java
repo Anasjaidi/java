4f04e83d5a17051be904858b1d741b83d984cf90
Anas jaidi <anas.jaidi@icloud.com>
2023-01-30 23:18:40 +0100

    print

--- main.java
+++
@@ -1,5 +0,0 @@
-public class main {
-  static public void main(String[] ergs) {
-    System.out.println("anas jaidi");
-  }
-}
---
+++ print.java
@@ -0,0 +1,6 @@
+public class print {
+  static public void main(String[] ergs) {
+    System.out.print("Hello, World!\n");
+    System.out.print("anas jaidi");
+  }
+}
