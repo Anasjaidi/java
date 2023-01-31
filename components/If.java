47a335cded17f5cd1d603a99a2b2ef8a139fc91c
anas jaidi <anas.jaidi@icloud.com>
2023-01-31 17:26:12 +0100

    add if statements

---
+++ components/If.java
@@ -0,0 +1,13 @@
+package components;
+
+public class If {
+  public static void main(String[] args) {
+    if (false) {
+      System.out.println("anas jaidi");
+    } else if (false) {
+      System.out.println("Robinhood");
+    } else {
+      return ;
+    }
+  }
+}
