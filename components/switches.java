a7fe20717c8fc70f325a1f513ea5950b2f6cb8d8
anas jaidi <anas.jaidi@icloud.com>
2023-01-31 18:39:59 +0100

    add switche statements

---
+++ components/switches.java
@@ -0,0 +1,23 @@
+package components;
+
+public class switches {
+  public static void main(String[] args) {
+    switch (12) {
+      case 12:
+        System.out.println("Anasjaidi");
+        break;
+    
+      default:
+        break;
+    }
+    String s = "anas";
+    switch (s) {
+      case "anas":
+        System.out.println("anas");
+        break;
+    
+      default:
+        break;
+    }
+  }
+}
