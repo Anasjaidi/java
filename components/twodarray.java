8cbdfda7656fc1fa162afaaaf2e787e047d0f069
anas jaidi <anas.jaidi@icloud.com>
2023-01-31 19:19:50 +0100

    add 2d string array

---
+++ components/twodarray.java
@@ -0,0 +1,11 @@
+package components;
+
+public class twodarray {
+  public static void main(String[] args) {
+    String[][] cars0 = new String[2][3];
+    int[][] nums = {{1,2,3},{3,4,5}};
+    cars0[1][0] = "anas";
+
+    
+  }
+}
