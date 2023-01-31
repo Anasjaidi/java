c942017ca63f6b81a3be5b9c9b75caa1166bb9df
anas jaidi <anas.jaidi@icloud.com>
2023-01-31 19:15:14 +0100

    add array

---
+++ components/arrays.java
@@ -0,0 +1,13 @@
+package components;
+
+public class arrays {
+  public static void main(String[] args) {
+    String[] cars0 = {"a", "b", "C"};
+    String[] cars1 = new  String[3];
+    cars1[0] = "A";
+    cars1[1] = "B";
+    cars1[2] = "C";
+    System.out.println(cars0.length);
+    
+  }
+}
