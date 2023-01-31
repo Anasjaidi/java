25273961034b41e25f11a769aef03b830386d133
anas jaidi <anas.jaidi@icloud.com>
2023-01-31 18:43:57 +0100

    add logical operators

---
+++ components/logicalOpeartors.java
@@ -0,0 +1,16 @@
+package components;
+
+public class logicalOpeartors {
+  public static void main(String[] args) {
+    // &&, ||, !
+    if (true && true) {
+      System.out.println("true && true");
+    }
+    if (true || false) {
+      System.out.println("true || false"); 
+    }
+    if (!false) {
+      System.out.println("!false");
+    }
+  }
+}
