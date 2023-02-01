5f826a6423825bcd5d01129fe4b293d074559935
Anas jaidi <anas.jaidi@icloud.com>
2023-02-01 23:18:30 +0100

    add array of objects

---
+++ components/ArrayOfObjects.java
@@ -0,0 +1,11 @@
+package components;
+
+import components.utils.Human;
+
+public class ArrayOfObjects {
+  public static void main(String[] args) {
+    Human[] hs = new Human[4];
+
+    hs[0] = new Human("A", 0);
+  }
+}
