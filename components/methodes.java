7bbdaf0f96f3f80ddc687afcff1f1181f2fcaacf
Anas jaidi <anas.jaidi@icloud.com>
2023-02-01 02:19:00 +0100

    methodes

---
+++ components/methodes.java
@@ -0,0 +1,11 @@
+package components;
+
+public class methodes {
+  public static void main(String[] args) {
+    System.out.println(inc(0));
+  }
+
+  static int inc(int n) {
+    return ++n;
+  }
+}
