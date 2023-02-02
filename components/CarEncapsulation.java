a7c8b5b0c4d496ca21c9416bb6d49133b3293ed3
Anas jaidi <anas.jaidi@icloud.com>
2023-02-02 23:48:09 +0100

    add encapsulation in java

---
+++ components/CarEncapsulation.java
@@ -0,0 +1,19 @@
+package components;
+
+public class CarEncapsulation {
+  private String model;
+  private int year;
+
+  public int getYear() {
+    return year;
+  }
+  public String getModel() {
+    return model;
+  }
+  public void setYear(int _year) {
+    year = _year;
+  }
+  public void setModel(String _model) {
+    model = _model;
+  }
+}
---
+++ components/encapsulation.java
@@ -0,0 +1,12 @@
+package components;
+
+public class encapsulation {
+  public static void main(String[] args) {
+    CarEncapsulation c1 = new CarEncapsulation();
+    c1.setModel("corvette");
+    c1.setYear(2020);
+    System.out.println(c1.getModel());
+    System.out.println(c1.getYear());
+    
+  }
+}
