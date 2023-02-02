944b3d65b6ada608032117783b6a3b70bec425f2
anas jaidi <anas.jaidi@icloud.com>
2023-02-02 21:30:01 +0100

    add acces specifiers

---
+++ components/accessModifiers.java
@@ -0,0 +1,14 @@
+package components;
+
+public class accessModifiers {
+  String s1;
+  public String s2;
+  protected String s3;
+  private String s4;
+  public static void main(String[] args) {
+    //? public: all the world
+    //? protectd: class, package, direved
+    //? no modifier: class, package
+    //? private: only class
+  }
+}
