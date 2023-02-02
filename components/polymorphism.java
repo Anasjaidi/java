3056b87863f9ee6c9d6cd123a3f52496c5119710
Anas jaidi <anas.jaidi@icloud.com>
2023-02-03 00:35:51 +0100

    add polymorphism

---
+++ components/polymorphism.java
@@ -0,0 +1,14 @@
+package components;
+
+import components.utils.Bike;
+import components.utils.vehicule;
+
+public class polymorphism {
+  public static void main(String[] args) {
+    Bike b =new Bike();
+    fnc(b);
+  }
+  static void fnc(vehicule b) {
+
+  }
+}
