a23c990486d6f32849986e3037c2f12b87e567e3
Anas jaidi <anas.jaidi@icloud.com>
2023-02-03 11:37:32 +0100

    add dynamic polymorphism

---
+++ components/dynamicPolymorphism.java
@@ -0,0 +1,10 @@
+package components;
+
+import components.utils.Bike;
+import components.utils.vehicule;
+
+public class dynamicPolymorphism {
+  public static void main(String[] args) {
+    vehicule a = new Bike();
+  }
+}
