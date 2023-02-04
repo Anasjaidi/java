17bbaeb722da24dc18aa7d850391e3118a5f563b
Anas jaidi <anas.jaidi@icloud.com>
2023-02-04 22:23:46 +0100

    finishing queues in java

--- DSA/data-structures/queue/queue.java
+++ DSA/data-structures/queue/queue.java
@@ -13,12 +13,41 @@ import java.util.Queue;
 public class queue {
 
   public static void main(String[] args) {
+
     Queue<String> queue = new LinkedList<String>();
+
+
     /*
     * queue includes only 3 methodes the rest is comme with LinkList class
     * offer: add elemnt to the end of queue
     * poll:  remove the first element in th queue
     * peek:  return first element
+    */
+
+
+    /*
+     * thes collection of methodes only return values in error case
+    */
+
+    queue.offer("java");
+    queue.offer("javascript");
+    queue.offer("typescript");
+
+    System.out.println(queue.peek()); // java
+    
+    String poped = queue.poll();
+    
+    System.out.println("poped: "+poped); // poped: java
+    
+    System.out.println(queue.size()); // 2
+    
+    System.out.println(queue.peek()); // javascript
+    
+    /*
+     * and the equveilant of these methodes that throws exception in error case
+     * add: add elemnt
+     * remove: pop element
+     * element: peek first element
     */
   }
 }
