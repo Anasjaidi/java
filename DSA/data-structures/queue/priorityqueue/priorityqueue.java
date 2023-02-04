8b01d771236dccf6c688315272a87fe9fbb9b327
Anas jaidi <anas.jaidi@icloud.com>
2023-02-04 22:50:07 +0100

    added queue with priorityqueue

---
+++ DSA/data-structures/queue/priorityqueue/priorityqueue.java
@@ -0,0 +1,13 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * ? priorityqueue
+ *    fifo ds that serves upon priority
+ *    element with high priority before elent with lower priority
+ */
+public class priorityqueue {
+  public static void main(String[] args) {
+    Queue<String> queue = new PriorityQueue<String>();
+  }
+}
