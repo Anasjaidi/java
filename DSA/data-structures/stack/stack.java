bcfa1ca2cc910e6a2d832412cb1a5318d8dbfc9b
Anas jaidi <anas.jaidi@icloud.com>
2023-02-04 21:45:38 +0100

    create and use empty methode on stack of strings in java

--- DSA/data-structures/stack/stack.java
+++ DSA/data-structures/stack/stack.java
@@ -1,9 +1,14 @@
+import java.util.Stack;
+
 /**
  * stack
  */
 public class stack {
 
   public static void main(String[] args) {
+    Stack<String> stack = new Stack<String>();
+
+    System.out.println(stack.empty());
     
   }
 }
