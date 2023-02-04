7bfcb3d5d78f10247c1f4b183599057213348f63
Anas jaidi <anas.jaidi@icloud.com>
2023-02-04 21:48:25 +0100

    push and call tostring of stack methode

--- DSA/data-structures/stack/stack.java
+++ DSA/data-structures/stack/stack.java
@@ -8,7 +8,14 @@ public class stack {
   public static void main(String[] args) {
     Stack<String> stack = new Stack<String>();
 
-    System.out.println(stack.empty());
+    System.out.println(stack.empty()); // true
     
+    stack.push("java");
+    stack.push("javascript");
+    stack.push("typescript");
+    
+    System.out.println(stack.empty()); // false
+
+    System.out.println(stack); // [java, javascript, typescript]
   }
 }
