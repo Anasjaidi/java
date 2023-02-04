2ed26ee548e7c06c5ac6e8c567495ec73ca2545a
Anas jaidi <anas.jaidi@icloud.com>
2023-02-04 22:58:43 +0100

    add reversed order strings priority que

--- DSA/data-structures/queue/priorityqueue/priorityqueue.java
+++ DSA/data-structures/queue/priorityqueue/priorityqueue.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -9,14 +11,25 @@ import java.util.Queue;
 public class priorityqueue {
   public static void main(String[] args) {
     Queue<Double> queue = new PriorityQueue<Double>();
-
+    
     queue.offer(0.5);
     queue.offer(2.5);
     queue.offer(1.0);
     queue.offer(10.8);
     queue.offer(3.8);
-
+    
     System.out.println(queue); // [0.5, 2.5, 1.0, 10.8, 3.8]
+    
+    Queue<String> queue1 = new PriorityQueue<String>(Collections.reverseOrder());
+
+
+    queue1.offer("A");
+    queue1.offer("C");
+    queue1.offer("Z");
+    queue1.offer("B");
+    queue1.offer("D");
+
+    System.out.println(queue1); // [Z, D, C, A, B]
     
   }
 }
