d12510cc344194a052a13b8fc7d83f0c29bd5bc3
Anas jaidi <anas.jaidi@icloud.com>
2023-02-05 22:04:31 +0100

    add all members

--- DSA/data-structures/DynamicArray/dynamicArray.java
+++ DSA/data-structures/DynamicArray/dynamicArray.java
@@ -2,14 +2,50 @@ import java.util.ArrayList;
 
 public class dynamicArray {
   int capacity = 10;
-  int size;
+  int size = 0;
   Object[] arr;
 
   public dynamicArray() {
     arr = new Object[capacity];
   }
+
+
   public dynamicArray(int _capacity) {
     arr = new Object[_capacity];
     capacity = _capacity;
+  }
+
+  public void add(Object _data) {
+    if (size <= capacity) grow();
+    arr[size++] = _data;
+  }
+
+  public void insert(int _i, Object _data) {
+
+  }
+
+  public void delete(Object data) {
+
+  }
+
+  public int search(Object data) {
+    return -1;
+  }
+
+  private void grow() {
+
+  }
+
+  private void shrink() {
+
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+  @Override
+  public String toString() {
+    // TODO Auto-generated method stub
+    return super.toString();
   }
 }
