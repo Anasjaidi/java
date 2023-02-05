b6e09bb5f50f1c6cc1d51ca78d9f56f51405a307
Anas jaidi <anas.jaidi@icloud.com>
2023-02-05 21:55:39 +0100

    add default constructor and capacity constructor

--- DSA/data-structures/DynamicArray/dynamicArray.java
+++ DSA/data-structures/DynamicArray/dynamicArray.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
 public class dynamicArray {
+  int capacity = 10;
+  int size;
+  Object[] arr;
 
+  public dynamicArray() {
+    arr = new Object[capacity];
+  }
+  public dynamicArray(int _capacity) {
+    arr = new Object[_capacity];
+    capacity = _capacity;
+  }
 }
