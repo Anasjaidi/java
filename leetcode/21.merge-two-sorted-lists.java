480d2d2905696b394bdaf85dd4c3c589e72cd766
Anas jaidi <anas.jaidi@icloud.com>
2023-02-08 00:21:01 +0100

    reverse list in java with iteration leetcode

---
+++ leetcode/21.merge-two-sorted-lists.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ */
+// public class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode() {}
+//     ListNode(int val) { this.val = val; }
+//     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// }
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode list = null;
+        // ListNode tmp1 = list1;
+        // ListNode tmp2 = list2;
+        // while (tmp1 != null || tmp2 != null) {
+        //     // if (tmp1.val > tmp2.val) {
+
+        //     // }
+        //     System.out.println(tmp1.val+" "+tmp2.val);
+        //     if (tmp1 != null) tmp1 = tmp1.next;
+        //     if (tmp2 != null) tmp2 = tmp1.next;
+
+        // }
+
+        return list;
+    } // 1 , 7 , 8, 19
+    // 1, 2, 3, 5, 7, 8,9
+}
+// @lc code=end
+
