edbfbd23e44966d5b0e6d8d54075153f8201ced8
Anas jaidi <anas.jaidi@icloud.com>
2023-02-07 22:42:32 +0100

    start implm a selectionsort++ function

--- DSA/Algorithms/sort/selection-sort/selectionSort.java
+++ DSA/Algorithms/sort/selection-sort/selectionSort.java
@@ -1,7 +1,7 @@
 public class selectionSort {
   public static void main(String[] args) {
     int[] arr = { 1, 9, 5, 6, 8, 2, 3, 7 };
-    selectionSort(arr);
+    sortMinMax(arr);
     for (int i = 0; i < arr.length; i++) {
 
       System.out.println(arr[i]);
@@ -20,6 +20,10 @@ public class selectionSort {
       arr[i] = arr[min];
       arr[min] = temp;
     }
+  }
+  
+  public static void sortMinMax(int[] arr) {
+    
   }
 
 }
