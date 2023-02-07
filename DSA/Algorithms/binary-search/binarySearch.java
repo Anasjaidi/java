4b61c39f5378e91878ef7c12977107bf75b326a3
Anas jaidi <anas.jaidi@icloud.com>
2023-02-07 02:01:44 +0100

    implm binary search more quickest than java builtin

--- DSA/Algorithms/binary-search/binarySearch.java
+++ DSA/Algorithms/binary-search/binarySearch.java
@@ -1,9 +1,39 @@
+import java.util.Arrays;
+
 /**
  * binarySearch
  */
 public class binarySearch {
 
   public static void main(String[] args) {
+    long[] arr = new long[(int)1e6];
+
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = i;
+    }
+    long startTime , endTime;
+  
+    startTime = System.nanoTime();
+    Arrays.binarySearch(arr, 420000);
+    endTime = System.nanoTime();
+    System.out.println("external binary search time: "+(endTime - startTime) / 1e6 + " ms");
     
+    startTime = System.nanoTime();
+    binarySearch(arr, 420000);
+    endTime = System.nanoTime();
+    System.out.println("internal binary search time: "+(endTime - startTime) / 1e6 + " ms");
+    
+  }
+
+  public static int binarySearch(long[] arr, long key) {
+    int low = 0, high = arr.length;
+
+    while (low <= high) {
+      int mid = (low + high) >> 1;
+      if (arr[mid] > key) high = mid - 1;
+      else if (arr[mid] < key) low = mid + 1;
+      else return mid;
+    }
+    return -1;
   }
 }
