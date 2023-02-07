fb57ee03a6e25ec085955ea0b030d423c059de31
Anas jaidi <anas.jaidi@icloud.com>
2023-02-07 22:35:23 +0100

    add selection sort

---
+++ DSA/Algorithms/sort/selection-sort/selectionSort.java
@@ -0,0 +1,26 @@
+public class selectionSort {
+  public static void main(String[] args) {
+    int[] arr = { 1, 9, 5, 6, 8, 2, 3, 7 };
+    selectionSort(arr);
+    for (int i = 0; i < arr.length; i++) {
+
+      System.out.println(arr[i]);
+    }
+
+  }
+
+  private static void selectionSort(int[]arr) {
+    int min;
+    for (int i = 0; i < arr.length - 1; i++) {
+      min = i;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[min] > arr[j]) min = j;
+      }
+      int temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+
+}
+
