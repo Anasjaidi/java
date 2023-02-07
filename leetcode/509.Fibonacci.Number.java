3910188ddbd1cff033a3d340bd8d89eae9736887
Anas jaidi <anas.jaidi@icloud.com>
2023-02-08 00:48:21 +0100

    finsh fib with iteration leetcode java

--- leetcode/509.Fibonacci.Number.java
+++ leetcode/509.Fibonacci.Number.java
@@ -1,7 +1,21 @@
 class Solution {
   public int fib(int n) {
+    // if (n == 0 || n == 1) return n;
+    // return (fib(n - 1) + fib(n - 2));
+    return (fibIter(n));
+  }
+
+  int fibIter(int n) {
     if (n == 0 || n == 1)
       return n;
-    return (fib(n - 1) + fib(n - 2));
+    int a = 1, b = 0, c = 0;
+    int i = 2;
+    while (i <= n) {
+      c = a + b;
+      b = a;
+      a = c;
+      i++;
+    }
+    return c;
   }
 }
