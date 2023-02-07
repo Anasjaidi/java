fadd597168ddbcf302507a89e81a0a9bc3c1c07e
Anas jaidi <anas.jaidi@icloud.com>
2023-02-07 23:59:27 +0100

    add insertion sort

---
+++ DSA/Algorithms/sort/insertion-sort/insertionSort.java
@@ -0,0 +1,23 @@
+public class insertionSort {
+  public static void main(String[] args) {
+    int[] arr = { 1, 9, 5, 6, 8, 2, 3, 7 };
+    insertionSort(arr);
+    for (int i = 0; i < arr.length; i++) {
+
+      System.out.println(arr[i]);
+    }
+  }
+  private static void insertionSort(int[]arr) {
+    int tmp;
+
+    for (int i = 0; i < arr.length - 1; i++) {
+      tmp = arr[i + 1];
+      for (int j = i; j >= 0; j--) {
+        if (arr[j] > tmp) {
+          arr[j + 1] = arr[j];
+          arr[j] = tmp;
+        }
+      }
+    }
+  }
+}
