6bbca25dfc52e27ad913aa866bc8a4d3a91067f4
anas jaidi <anas.jaidi@icloud.com>
2023-02-09 17:46:10 +0100

    add sort a array problem with merge sort algo

---
+++ leetcode/912.Sort.an.Array.java
@@ -0,0 +1,29 @@
+class Solution {
+  public int[] sortArray(int[] nums) {
+    mergeSort(nums);
+    return nums;
+  }
+
+  void mergeSort(int[] arr) {
+    if (arr.length <= 1)
+      return;
+    int[] rhs = Arrays.copyOfRange(arr, 0, arr.length / 2), lhs = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
+    mergeSort(lhs);
+    mergeSort(rhs);
+    merge(lhs, rhs, arr);
+  }
+
+  void merge(int[] lhs, int[] rhs, int[] arr) {
+    int i = 0, l = i, r = i;
+    while (l < lhs.length && r < rhs.length) {
+      if (lhs[l] < rhs[r])
+        arr[i++] = lhs[l++];
+      else
+        arr[i++] = rhs[r++];
+    }
+    while (l < lhs.length)
+      arr[i++] = lhs[l++];
+    while (r < rhs.length)
+      arr[i++] = rhs[r++];
+  }
+}
