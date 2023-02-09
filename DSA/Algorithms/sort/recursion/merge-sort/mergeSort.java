41617f814b39a25c8c5e20442c100b5b3fe22908
anas jaidi <anas.jaidi@icloud.com>
2023-02-09 17:14:59 +0100

    finish merge sort algoritheme

--- DSA/Algorithms/sort/recursion/merge-sort/mergeSort.java
+++ DSA/Algorithms/sort/recursion/merge-sort/mergeSort.java
@@ -3,11 +3,31 @@ import java.util.Arrays;
 public class mergeSort {
   public static void main(String[] args) {
     int[] arr = {9,6,3,1,4,7,5,2,8};
-    mergeSort(Arrays.copyOfRange(arr, 0, arr.length / 2), Arrays.copyOfRange(arr, arr.length / 2, arr.length));
-  }
-  private static void mergeSort(int[] lhs, int[] rhs) {
-    System.out.println(Arrays.toString(lhs));
-    System.out.println(Arrays.toString(rhs));
+    mergeSort(arr);
+    System.out.println(Arrays.toString(arr));
     
+  }
+  private static void mergeSort(int[] arr) {
+    if (arr.length <= 1) return;
+    int[] lhs = Arrays.copyOfRange(arr, 0, arr.length / 2), rhs = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
+    mergeSort(lhs);
+    mergeSort(rhs);
+    merge(lhs, rhs, arr);
+  }
+  private static void merge(int[] lhs, int[] rhs, int[] arr) {
+    int i = 0, r = i, l = i;
+    System.out.println("rhs length: "+rhs.length);
+    System.out.println("lhs length: "+lhs.length);
+    
+    while (l < lhs.length && r < rhs.length) {
+      if (lhs[l] < rhs[r]) arr[i++] = lhs[l++];
+      else arr[i++] = rhs[r++];
+    }
+    while (l < lhs.length)
+    arr[i++] = lhs[l++];
+    while (r < rhs.length)
+    arr[i++] = rhs[r++];
+    System.out.println(Arrays.toString(arr));
+    return;
   }
 }
