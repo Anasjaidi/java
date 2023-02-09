ba3c1b192a27bbc0ed0b741e119bbc39e0a29a2c
Anas jaidi <anas.jaidi@icloud.com>
2023-02-09 02:45:00 +0100

    merge teo sorted lists with recursion in java

--- leetcode/21.merge-two-sorted-lists.java
+++ leetcode/21.merge-two-sorted-lists.java
@@ -1,39 +1,25 @@
-/*
- * @lc app=leetcode id=21 lang=java
- *
- * [21] Merge Two Sorted Lists
- */
-
-// @lc code=start
 /**
  * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
-// public class ListNode {
-//     int val;
-//     ListNode next;
-//     ListNode() {}
-//     ListNode(int val) { this.val = val; }
-//     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-// }
-
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode list = null;
-        // ListNode tmp1 = list1;
-        // ListNode tmp2 = list2;
-        // while (tmp1 != null || tmp2 != null) {
-        //     // if (tmp1.val > tmp2.val) {
-
-        //     // }
-        //     System.out.println(tmp1.val+" "+tmp2.val);
-        //     if (tmp1 != null) tmp1 = tmp1.next;
-        //     if (tmp2 != null) tmp2 = tmp1.next;
-
-        // }
-
-        return list;
-    } // 1 , 7 , 8, 19
-    // 1, 2, 3, 5, 7, 8,9
+        ListNode tmp;
+        if (list1 == null && list2 == null) return null;
+        else if (list1 == null) return list2;
+        else if (list2 == null) return list1;
+        if (list1.val >= list2.val) {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        } else {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }}
+    
 }
-// @lc code=end
-
