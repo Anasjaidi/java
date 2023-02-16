427cdccf4b2801e612459b76a0002db9d3b2314b
Anas jaidi <anas.jaidi@icloud.com>
2023-02-16 03:24:44 +0100

    hashmap java ds

--- DSA/data-structures/HashTable/hashmap.java
+++ DSA/data-structures/HashTable/hashmap.java
@@ -7,5 +7,22 @@ public class hashmap {
 
   public static void main(String[] args) {
     HashMap<String, Integer> a = new HashMap<String, Integer>();
+    a.put("anas", 0);
+
+    System.out.println(a.get("anas"));
+    
+    System.out.println(a.containsKey("anas"));
+    
+    System.out.println(a.containsValue(0));
+    
+    a.replace("jaidi", 12);
+
+    System.out.println(a.containsKey("jaidi"));
+
+
+    a.put("anas", 7);
+    
+    a.putIfAbsent("jaidi", 12);
+    System.out.println(a);
   }
 }
