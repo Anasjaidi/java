d2e120ec4ecc806edb8de7a034976d710fe65a05
Anas jaidi <anas.jaidi@icloud.com>
2023-02-16 03:15:51 +0100

    create hash map

---
+++ DSA/data-structures/HashTable/hashmap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+/**
+ * hashmap
+ */
+public class hashmap {
+
+  public static void main(String[] args) {
+    HashMap<String, Integer> a = new HashMap<String, Integer>();
+  }
+}
