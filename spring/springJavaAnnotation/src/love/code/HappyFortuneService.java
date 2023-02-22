b3004d7efeb44cc0eb01999bc87a22a8ed7bd474
anas jaidi <anas.jaidi@icloud.com>
2023-02-22 19:19:16 +0100

    add constructor for dependcy injection

---
+++ spring/springJavaAnnotation/src/love/code/HappyFortuneService.java
@@ -0,0 +1,11 @@
+package love.code;
+
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return "Happy Daily Fortune";
+	}
+
+}
