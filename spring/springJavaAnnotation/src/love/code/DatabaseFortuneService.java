1c9c9926d834b4cb63e01d0028a48cf895d79072
Anas jaidi <anas.jaidi@icloud.com>
2023-02-25 19:05:55 +0100

    add db fortune service

---
+++ spring/springJavaAnnotation/src/love/code/DatabaseFortuneService.java
@@ -0,0 +1,11 @@
+package love.code;
+
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
