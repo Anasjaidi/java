6acace72c11c8fdfca3109c080decc610b95b4f8
Anas jaidi <anas.jaidi@icloud.com>
2023-02-25 19:03:57 +0100

    add random fortune service

---
+++ spring/springJavaAnnotation/src/love/code/RandomService.java
@@ -0,0 +1,11 @@
+package love.code;
+
+public class RandomService implements FortuneService {
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
