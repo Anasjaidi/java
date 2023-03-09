58318bb649852ea4abe8db33e8811d5988f823ec
Anas jaidi <anas.jaidi@icloud.com>
2023-03-09 12:54:08 +0100

    fix some issues with hibernate insert

--- spring/hibernate/src/love/code/hibernate/CreateStudent.java
+++ spring/hibernate/src/love/code/hibernate/CreateStudent.java
@@ -1,9 +1,61 @@
 package love.code.hibernate;
 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.sql.ordering.antlr.Factory;
+
+
+import org.hibernate.*;
+
+import love.code.hibernate.entity.Student;
+
 public class CreateStudent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/**
+		 * start session factory
+		 * */
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		/**
+		 * create session
+		 * */
+		
+		Session session = factory.getCurrentSession();
+		
+		
+		try {
+			/**
+			 * Create a student object 
+			 * */
+			
+				Student student = new Student("anas", "jaidi", "anas.jaidi@icloud.com");
+			
+			/**
+			 * create a transaction
+			 * */
+				
+				session.beginTransaction();
+			
+			/**
+			 * save the student object
+			 * */
+				
+				session.save(student);
+			
+			/**
+			 * commit the transaction
+			 * */
+				session.getTransaction().commit();
+			
+				
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
--- spring/hibernate/src/love/code/hibernate/entity/Student.java
+++ spring/hibernate/src/love/code/hibernate/entity/Student.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "studen")
+@Table(name = "student")
 public class Student {
 	@Id
 	@Column(name = "id")
