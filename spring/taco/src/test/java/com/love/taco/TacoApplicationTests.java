bd7b8f5a941ae66cb00ca2e88f9124eddee461d7
Anas jaidi <anas.jaidi@icloud.com>
2023-03-18 01:01:24 +0100

    finish first rndpoint

--- spring/taco/src/test/java/com/love/taco/TacoApplicationTests.java
+++ spring/taco/src/test/java/com/love/taco/TacoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.love.taco;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static
+org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static
+org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static
+org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static
+org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class TacoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+import com.love.taco.controller.TacoHomeController;
+@RunWith(SpringRunner.class)
+@WebMvcTest(TacoHomeController.class)
+public class TacoApplicationTests {
+ @Autowired
+ private MockMvc mockMvc;
+ @Test
+ public void testHomePage() throws Exception {
+ mockMvc.perform(get("/"))
+ .andExpect(status().isOk())
+ .andExpect(view().name("home"))
+ .andExpect(content().string(
+ containsString("Welcome to...")));
+ }
 }
