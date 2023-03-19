32a73a9ac1f350b2e91e83851572032224c1dca0
Anas jaidi <anas.jaidi@icloud.com>
2023-03-19 16:29:03 +0000

    store form submition data in several models

--- spring/tacos/src/main/java/com/love/tacos/controller/order/orderController.java
+++ spring/tacos/src/main/java/com/love/tacos/controller/order/orderController.java
@@ -1,18 +1,27 @@
 package com.love.tacos.controller.order;
 
+import com.love.tacos.model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class orderController {
 
     @GetMapping("/current")
     public String orderCurrent(Model model) {
+        model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processForm(Order order, Model model) {
+        log.info(order.toString());
+        return "redirect:/";
     }
 }
