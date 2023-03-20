d22a6fb870facfb856984af9f30207e4319920c4
anas jaidi <anas.jaidi@icloud.com>
2023-03-20 17:40:22 +0000

    add simple viewController

--- spring/tacos/src/main/java/com/love/tacos/controller/TacoHomeController.java
+++ spring/tacos/src/main/java/com/love/tacos/controller/TacoHomeController.java
@@ -1,14 +1,22 @@
 package com.love.tacos.controller;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Controller
-public class TacoHomeController {
+//@Controller
+@Configuration
+public class TacoHomeController implements WebMvcConfigurer {
 
-	@GetMapping("/")
-	public String  getHoume() {
-		return "home";
+//	@GetMapping("/")
+//	public String  getHoume() {
+//		return "home";
+//	}
+	@Override
+	public void  addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
 	}
 
 
