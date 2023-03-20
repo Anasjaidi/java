7c9583786e273c5c548866bfbd11fe1ad4aee2c6
Anas jaidi <anas.jaidi@icloud.com>
2023-03-20 11:38:55 +0000

    Finish Update Crud operation

--- spring/sboot/src/main/java/com/love/sboot/controllers/CustomerController.java
+++ spring/sboot/src/main/java/com/love/sboot/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -30,5 +31,21 @@ public class CustomerController {
     @DeleteMapping("{_id}")
     public void deleteCustomer(@PathVariable("_id") Integer _ID) {
         customerRepository.deleteById(_ID);
+    }
+
+    @PutMapping("{_ID}")
+
+    public Customer updateCustomer(@RequestBody RequestCustomerBody req, @PathVariable("_ID") Integer _id) {
+        Optional<Customer> added = customerRepository.findById(_id);
+        Optional<Customer> optionalCustomer = customerRepository.findById(_id);
+        if (added.isEmpty()) {
+            return new Customer();
+        }
+        Customer customer = added.get();
+        customer.setName(req.name);
+        customer.setAge(req.age);
+        customer.setEmail(req.email);
+        customerRepository.save(customer);
+        return new Customer();
     }
 }
