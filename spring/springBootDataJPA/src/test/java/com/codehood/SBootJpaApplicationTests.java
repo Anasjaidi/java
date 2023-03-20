46d7dc2ea6ebfc6968635e678f41cc8ba4bdeb9d
anas jaidi <anas.jaidi@icloud.com>
2023-03-20 20:45:48 +0000

    start jpa course project files setup

---
+++ spring/springBootDataJPA/src/test/java/com/codehood/SBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
