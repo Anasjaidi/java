382925f7eb768fe11156064ad4143f8844681ad6
Anas jaidi <anas.jaidi@icloud.com>
2023-03-21 00:27:38 +0000

    add columns metadata

--- spring/springBootDataJPA/src/main/java/com/codehood/entity/Student.java
+++ spring/springBootDataJPA/src/main/java/com/codehood/entity/Student.java
@@ -1,17 +1,48 @@
 package com.codehood.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
-@Entity
+@Entity(name = "student")
 public class Student {
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
 
+    @Column(
+            name = "age"
+    )
     private Integer age;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true
+    )
     private String email;
 }
