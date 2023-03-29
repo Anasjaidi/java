21f40beb749edf1eceb53f1a59d1cf7c5cb29d35
anas jaidi <anas.jaidi@icloud.com>
2023-03-29 15:59:27 +0000

    add first endpoint for rest api

---
+++ spring/spring3OverView/src/main/java/com/hood/spring3overview/Rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.hood.spring3overview.Rest;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
