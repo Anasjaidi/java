714c6cef51ae18c3f8b67739ed1ae670263e4c05
anas jaidi <anas.jaidi@icloud.com>
2023-03-29 15:52:44 +0000

    start spring 3

---
+++ spring/spring3OverView/src/main/java/com/hood/spring3overview/Spring3OverViewApplication.java
@@ -0,0 +1,13 @@
+package com.hood.spring3overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3OverViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3OverViewApplication.class, args);
+    }
+
+}
---
+++ spring/spring3OverView/src/test/java/com/hood/spring3overview/Spring3OverViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hood.spring3overview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3OverViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
