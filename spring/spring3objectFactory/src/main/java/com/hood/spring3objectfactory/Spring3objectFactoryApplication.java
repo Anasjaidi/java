7c933cf4f39e50f4b28e71b3ee9b0e04ac6316f4
anas jaidi <anas.jaidi@icloud.com>
2023-03-30 18:15:55 +0000

    start spring object factory (ibversion of control, dependecy injection)

---
+++ spring/spring3objectFactory/src/main/java/com/hood/spring3objectfactory/Spring3objectFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.hood.spring3objectfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3objectFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3objectFactoryApplication.class, args);
+    }
+
+}
---
+++ spring/spring3objectFactory/src/test/java/com/hood/spring3objectfactory/Spring3objectFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hood.spring3objectfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3objectFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
