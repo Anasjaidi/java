6d9ae13eae0099fca7f11b90285f298404e87840
anas jaidi <anas.jaidi@icloud.com>
2023-03-30 15:49:38 +0000

    inject values from the conf file 'app.propreties|yml'

--- spring/spring3OverView/src/main/java/com/hood/spring3overview/Rest/FunRestController.java
+++ spring/spring3OverView/src/main/java/com/hood/spring3overview/Rest/FunRestController.java
@@ -1,18 +1,28 @@
 package com.hood.spring3overview.Rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-
+    @Value("${conf.name}")
+    private String author;
     @RequestMapping("/")
     public String sayHello() {
         return "Hello, World, There!";
     }
     @RequestMapping("/hi")
     public String sayHi() {
-        return "Hello!";
+        return "Hello " + author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
