eb212681c0fa2f1bfb020aad344aa5770362d064
anas jaidi <anas.jaidi@icloud.com>
2023-03-31 15:31:04 +0000

    test prototype scope for beans

--- spring/spring3objectFactory/src/main/java/com/hood/spring3objectfactory/Coach/CricketCoach/CricketCoach.java
+++ spring/spring3objectFactory/src/main/java/com/hood/spring3objectfactory/Coach/CricketCoach/CricketCoach.java
@@ -1,14 +1,16 @@
 package com.hood.spring3objectfactory.Coach.CricketCoach;
 
 import com.hood.spring3objectfactory.Coach.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
 @Lazy
-
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("in constructor: " + getClass().getSimpleName());
