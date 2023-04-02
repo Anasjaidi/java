29e52066786e8a8c3032f5dc0ad9d80f0e25f445
anas jaidi <anas.jaidi@icloud.com>
2023-04-02 15:58:29 +0000

    push to remote

---
+++ spring/spring3DataAccess/src/test/java/com/hood/spring3dataaccess/Spring3DataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hood.spring3dataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3DataAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
