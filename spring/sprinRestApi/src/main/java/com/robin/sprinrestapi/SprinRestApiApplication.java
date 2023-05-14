e900f779e0d942689009f6cc64fec83d2bb381df
Anas jaidi <anas.jaidi@icloud.com>
2023-05-14 22:35:50 +0100

    init api home module

---
+++ spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/SprinRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.robin.sprinrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinRestApiApplication.class, args);
+    }
+
+}
---
+++ spring/sprinRestApi/src/test/java/com/robin/sprinrestapi/SprinRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.sprinrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
