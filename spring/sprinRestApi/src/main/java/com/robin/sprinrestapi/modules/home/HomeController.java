4e8384b866921b4c0ce77ce4b3fa886b754f0e66
Anas jaidi <anas.jaidi@icloud.com>
2023-05-15 02:29:31 +0100

    add path vars

--- spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/home/HomeController.java
+++ spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/home/HomeController.java
@@ -1,14 +1,31 @@
 package com.robin.sprinrestapi.modules.home;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Data
+class Student {
+    private String firsName;
+    private String lastName;
+
+    private int index;
+    public Student(String firsName, String lastName, int index) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.index = index;
+    }
+}
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/students")
 public class HomeController {
-    @GetMapping
-    public String Home() {
-        return "Hello, World!.";
+    @GetMapping("/{index}")
+    public Student Home(@PathVariable int index) {
+        Student s1 = new Student("anas", "jaidi", index);
+        return s1;
     }
 }
