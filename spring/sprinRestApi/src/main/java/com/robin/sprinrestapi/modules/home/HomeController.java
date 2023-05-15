e2e85536380393f25c70af217b6bf9250035ac76
Anas jaidi <anas.jaidi@icloud.com>
2023-05-15 02:45:45 +0100

    impl find One and find All

--- spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/home/HomeController.java
+++ spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/home/HomeController.java
@@ -1,11 +1,13 @@
 package com.robin.sprinrestapi.modules.home;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -13,19 +15,31 @@ class Student {
     private String firsName;
     private String lastName;
 
-    private int index;
-    public Student(String firsName, String lastName, int index) {
+    private int id;
+    public Student(String firsName, String lastName, int id) {
         this.firsName = firsName;
         this.lastName = lastName;
-        this.index = index;
+        this.id = id;
     }
 }
 @RestController
 @RequestMapping("/api/v1/students")
 public class HomeController {
+    private List<Student> students;
+    @PostConstruct
+    public void fillStudents() {
+
+        this.students = new ArrayList<>();
+
+        students.add(new Student("anas", "jaidi", 1));
+        students.add(new Student("robin", "hood", 2));
+    }
+    @GetMapping("/")
+    public List<Student> getAllStudents() {
+        return students;
+    }
     @GetMapping("/{index}")
-    public Student Home(@PathVariable int index) {
-        Student s1 = new Student("anas", "jaidi", index);
-        return s1;
+    public Student getStudentById(@PathVariable int index) {
+        return students.get(index);
     }
 }
