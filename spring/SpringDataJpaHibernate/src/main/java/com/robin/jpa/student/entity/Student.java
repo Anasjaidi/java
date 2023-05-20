c482336d3fbb9c25aa81a05674f8b32016ccb132
Anas jaidi <anas.jaidi@icloud.com>
2023-05-20 14:55:39 +0100

    generate seq

--- spring/SpringDataJpaHibernate/src/main/java/com/robin/jpa/student/entity/Student.java
+++ spring/SpringDataJpaHibernate/src/main/java/com/robin/jpa/student/entity/Student.java
@@ -1,21 +1,20 @@
 package com.robin.jpa.student.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "Student")
 @Table(name = "students")
 @Data
 @AllArgsConstructor(staticName = "builder")
 @NoArgsConstructor
 public class Student {
     @Id
-    private int id;
+    @SequenceGenerator(name = "student_seq", sequenceName = "student_sq_name", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sq_name")
+    private Long id;
 
     @Column(name = "first_name")
     private String firstName;
@@ -25,4 +24,8 @@ public class Student {
 
     @Column(name = "email")
     private String email;
+
+    @Column(name = "age")
+
+    private Integer age;
 }
