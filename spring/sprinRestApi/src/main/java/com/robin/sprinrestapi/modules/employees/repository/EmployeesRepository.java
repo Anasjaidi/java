f08607246714942e294590fda568bb22c1626879
Anas jaidi <anas.jaidi@icloud.com>
2023-05-20 21:21:57 +0100

    add first dao methode

--- spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/employees/repository/EmployeesRepository.java
+++ spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/employees/repository/EmployeesRepository.java
@@ -1,12 +1,23 @@
 package com.robin.sprinrestapi.modules.employees.repository;
 
 import com.robin.sprinrestapi.modules.employees.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class EmployeesRepository implements EmployeeRepositoryDAO{
+    private EntityManager manager;
+    @Autowired
+    public EmployeesRepository(EntityManager manager) {
+        this.manager = manager;
+    }
     @Override
     public List<Employee> findAll() {
-        return null;
+        TypedQuery<Employee> query = manager.createQuery("from Employee", Employee.class);
+        return query.getResultList();
     }
 }
