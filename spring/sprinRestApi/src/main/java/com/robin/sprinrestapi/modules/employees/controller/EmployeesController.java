b98ba449ed82d8239f699cfcc1d0a0cefb4fa37b
Anas jaidi <anas.jaidi@icloud.com>
2023-05-20 21:24:44 +0100

    add controler

--- spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/employees/controller/EmployeesController.java
+++ spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/employees/controller/EmployeesController.java
@@ -1,9 +1,24 @@
 package com.robin.sprinrestapi.modules.employees.controller;
 
+import com.robin.sprinrestapi.modules.employees.entity.Employee;
+import com.robin.sprinrestapi.modules.employees.repository.EmployeeRepositoryDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeesController {
+    EmployeeRepositoryDAO repository;
+    @Autowired
+    public EmployeesController(EmployeeRepositoryDAO repository) {
+        this.repository = repository;
+    }
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
 }
