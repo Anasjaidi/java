0cd435ac790bc9ce520d635492c60daa70bfd8be
Anas jaidi <anas.jaidi@icloud.com>
2023-05-20 23:06:02 +0100

    clean the module

--- spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/employees/controller/EmployeesController.java
+++ spring/sprinRestApi/src/main/java/com/robin/sprinrestapi/modules/employees/controller/EmployeesController.java
@@ -2,6 +2,7 @@ package com.robin.sprinrestapi.modules.employees.controller;
 
 import com.robin.sprinrestapi.modules.employees.entity.Employee;
 import com.robin.sprinrestapi.modules.employees.repository.EmployeeRepositoryDAO;
+import com.robin.sprinrestapi.modules.employees.service.EmployeesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeesController {
-    EmployeeRepositoryDAO repository;
+    EmployeesService service;
     @Autowired
-    public EmployeesController(EmployeeRepositoryDAO repository) {
-        this.repository = repository;
+    public EmployeesController(EmployeesService service) {
+        this.service = service;
     }
     @GetMapping
     public List<Employee> getAllEmployees() {
-        return repository.findAll();
+        return service.findAll();
     }
 }
