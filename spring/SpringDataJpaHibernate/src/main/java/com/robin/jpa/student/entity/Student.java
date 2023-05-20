a265a384404fa618df40eafef2a9cb98698e0c17
Anas jaidi <anas.jaidi@icloud.com>
2023-05-20 12:43:05 +0100

    define entity

---
+++ spring/SpringDataJpaHibernate/src/main/java/com/robin/jpa/student/entity/Student.java
@@ -0,0 +1,28 @@
+package com.robin.jpa.student.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@AllArgsConstructor(staticName = "builder")
+@NoArgsConstructor
+public class Student {
+    @Id
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
