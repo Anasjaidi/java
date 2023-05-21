85393f4277a2f71f8cafec7adf37a46f01caec33
Anas jaidi <anas.jaidi@icloud.com>
2023-05-21 18:47:22 +0100

    add basic rbac

--- spring/SpringSecurity/src/main/java/com/robin/springsecurity/config/BasicSecurityConfig.java
+++ spring/SpringSecurity/src/main/java/com/robin/springsecurity/config/BasicSecurityConfig.java
@@ -2,9 +2,12 @@ package com.robin.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicSecurityConfig {
@@ -15,6 +18,31 @@ public class BasicSecurityConfig {
                 "admin"
         ).build();
 
-        return new InMemoryUserDetailsManager(robin);
+        UserDetails anas =  User.builder().username("anas").password("{noop}anas").roles(
+                "manager"
+        ).build();
+
+        UserDetails jaidi =  User.builder().username("jaidi").password("{noop}jaidi").roles(
+                "EMPLOYEE"
+        ).build();
+
+        return new InMemoryUserDetailsManager(robin, anas, jaidi);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(req -> {
+            req
+                    .requestMatchers(HttpMethod.GET, "/api/v1/employees/**").hasAnyRole("EMPLOYEE", "admin")
+                    .requestMatchers(HttpMethod.GET, "/api/v1/employees").hasAnyRole("EMPLOYEE", "admin")
+                    .requestMatchers(HttpMethod.PATCH, "/api/v1/employees/**").hasAnyRole("manager", "admin")
+                    .requestMatchers(HttpMethod.POST, "/api/v1/employees").hasAnyRole("manager", "admin")
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/employees/**").hasAnyRole("admin");
+        });
+        http.httpBasic();
+
+        http.csrf().disable();
+        return http.build();
     }
 }
