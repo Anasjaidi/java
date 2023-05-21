4c9a5047a35c3f89aed1e14972eb4fee94f9ce2e
Anas jaidi <anas.jaidi@icloud.com>
2023-05-21 20:41:16 +0100

    create users entity

---
+++ spring/SpringSecurityJwt/src/main/java/com/robin/springsecurityjwt/User/entity/User.java
@@ -0,0 +1,26 @@
+package com.robin.springsecurityjwt.User.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
